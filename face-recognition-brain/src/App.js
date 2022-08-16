87bf6097cd66280a1b53a5a7c4b9c701f55956e6
ruks2022 <shaikhruksar2022@gmail.com>
2022-08-16 18:36:40 +0530

    Version-1

---
+++ face-recognition-brain/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Navigaton from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigaton />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
+
+    </div>
+  );
+}
+
+export default App;
